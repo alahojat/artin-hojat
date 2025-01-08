534d48f013abca9772cf0f354f448bd8511b7efb
Ala Hojat <alahojatjalali@gmail.com>
2025-01-08 18:39:30 +0100

    feat: :construction: move contactpage input fields to own component

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,7 +1,18 @@
+import { Input } from "../components/StyledInput";
+
 export const ContactPage = () => {
   return (
     <>
-      <h1>How to contact me</h1>
+      <section className="bg-steel p-4">
+        <h2 className="subheader subheader-light">How to contact me</h2>
+        <p className="body body-light">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <Input></Input>
+      </section>
     </>
   );
 };
