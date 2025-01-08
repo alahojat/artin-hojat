0b7d7a3761b74b950b377a670a7c5c5de048550a
Ala Hojat <alahojatjalali@gmail.com>
2025-01-08 18:38:46 +0100

    chore: :construction: update footer draft

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,22 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className="bg-orange p-4">
+        <div className="footer-container">
+          <div className="footer-top">
+            <div className="footer-follow">
+              <p className="footer">
+                <a href="#">Facebook</a> <a href="#">Twitter</a>
+                <a href="#">Instagram</a>
+              </p>
+            </div>
+          </div>
+
+          <div className="footer-bottom">
+            <p className="copyright">&copy; 2025 Artin Hojat</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
