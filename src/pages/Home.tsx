e7bad4160e2b6762c4994a61b8fec68e38b8c76d
Ala Hojat <alahojatjalali@gmail.com>
2025-01-08 18:17:39 +0100

    chore: :construction: add placeholder content for testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <section className="bg-ice sm:p-4 lg:p-6">
+        <h1 className="header header-light">Header</h1>
+        <h2 className="subheader">Subheader desktop</h2>
+        <p className="body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </section>
     </>
   );
 };
