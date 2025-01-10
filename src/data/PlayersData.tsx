088378ca0e97679870f66f6fff92376b17b103e9
Ala Hojat <alahojatjalali@gmail.com>
2025-01-10 22:12:59 +0100

    chore: :card_file_box: add data for each player

---
+++ src/data/PlayersData.tsx
@@ -0,0 +1,65 @@
+import melwin from "../assets/melwin-pantzar.png";
+import elliot from "../assets/elliot.jpg";
+import william from "../assets/william.png";
+import amanda from "../assets/amanda-zahui.jpg";
+import bobi from "../assets/bobi-klintman.jpg";
+import pelle from "../assets/pelle-larsson.jpg";
+
+export const players = [
+  {
+    id: 1,
+    title: "Elliot Cadeau",
+    description: "Skill Training, Strength and conditioning ",
+    url: elliot,
+    thumbnail: "/assets/images/thumbnails/city-skyline_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "Ala Hojat",
+  },
+  {
+    id: 2,
+    title: "Melwin Pantzar",
+    description: "Skill Training, Strength and conditioning ",
+    url: melwin,
+    thumbnail: "/assets/images/thumbnails/sunset_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    alt: "image",
+    photographer: "Ala Hojat",
+  },
+
+  {
+    id: 3,
+    title: "William",
+    description: "Skill Training, Strength and conditioning ",
+    url: william,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "Ala Hojat",
+  },
+  {
+    id: 4,
+    title: "Amanda Zahui B.",
+    description: "Skill Training, Strength and conditioning ",
+    url: amanda,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "?",
+  },
+  {
+    id: 5,
+    title: "Bobi Klintman",
+    description: "Skill Training, Strength and conditioning ",
+    url: bobi,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "?",
+  },
+  {
+    id: 6,
+    title: "Pelle Larsson",
+    description: "Skill Training, Strength and conditioning ",
+    url: pelle,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "?",
+  },
+];
