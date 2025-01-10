e30a50cd6a21ca3e832e734ccaa6c1f3b5596aef
Ala Hojat <alahojatjalali@gmail.com>
2025-01-10 14:47:43 +0100

    chore: :art: add player sections

--- src/pages/PlayerPage.tsx
+++ src/pages/PlayerPage.tsx
@@ -1,8 +1,27 @@
+import blueCourt from "../assets/edgar-chaparro.jpg";
+import william from "../assets/william.png";
+import melwin from "../assets/melwin-pantzar.png";
+import elliot from "../assets/elliot.jpg";
+
 export const PlayerPage = () => {
   return (
     <>
-      <section className="bg-steel p-4">
-        <h1>Players I work with</h1>
+      <section className="bg-steel">
+        <section className="relative">
+          <img className="h-full w-full object-cover" src={blueCourt} alt="" />
+          <section className="absolute inset-0 flex items-end p-4">
+            <h2 className="subheader">Optimize your potential</h2>
+          </section>
+        </section>
+        <section>
+          <img src={william} alt="" />
+        </section>
+        <section>
+          <img src={melwin} alt="" />
+        </section>
+        <section>
+          <img src={elliot} alt="" />
+        </section>
       </section>
     </>
   );
