a1138a31ce88de9a841ea75f67a3ad00e508fd0e
Ala Hojat <alahojatjalali@gmail.com>
2025-01-10 22:18:11 +0100

    feat: :construction: add hero page draft and implement logic

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,15 +1,30 @@
-import { Outlet, useRouteError } from "react-router-dom";
+import { Outlet, useLocation, useRouteError } from "react-router-dom";
 import { NotFound } from "./NotFound";
 import { Footer } from "./Footer";
 import { Menu } from "./Menu";
+import { Hero } from "./Hero";
+import { useState } from "react";
 
 export const Layout = () => {
   const error = useRouteError();
+  const location = useLocation();
+  const [showHero, setShowHero] = useState(location.pathname === "/");
+
+  const handleHeroDismiss = () => {
+    setShowHero(false);
+  };
+
   return (
     <>
-      <Menu></Menu>
-      <main className="header-sm">{error ? <NotFound /> : <Outlet />}</main>
-      <Footer></Footer>
+      {showHero ? (
+        <Hero onDismiss={handleHeroDismiss} />
+      ) : (
+        <>
+          <Menu />
+          <main className="header-sm">{error ? <NotFound /> : <Outlet />}</main>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
