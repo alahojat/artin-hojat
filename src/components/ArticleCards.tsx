b10c6d7d06bdbeae47058d59ea230a228478a15a
Ala Hojat <alahojatjalali@gmail.com>
2025-01-10 22:24:46 +0100

    feat: :sparkles: add articles presentation page with mock data

---
+++ src/components/ArticleCards.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import { articles } from "../data/MockArticlesData";
+
+export const ArticleCards = () => {
+  const handleArticleClick = () => {
+    console.log("is this working");
+    <NavLink to={"/articles/:id"}></NavLink>;
+  };
+  return (
+    <section
+      className="grid cursor-pointer grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4"
+      onClick={handleArticleClick}
+    >
+      {articles.map((article) => (
+        <div key={article.id} className="article-card mt-6 p-2">
+          <p className="body m-0">{article.dateCreated}</p>
+          <img
+            src={article.img}
+            alt={article.title}
+            className="w-full object-cover sm:h-52 lg:h-72"
+          />
+          <h3 className="title">{article.title}</h3>
+          <p className="body">{article.body}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -1,8 +1,11 @@
+import { ArticleCards } from "../components/ArticleCards";
+
 export const ArticlesPage = () => {
   return (
     <>
-      <section className="bg-ice max-w-7xl p-4 pt-24">
+      <section className="max-w-7xl bg-ice p-4 pt-24">
         <h2 className="subheader">The art in Science</h2>
+        <ArticleCards></ArticleCards>
       </section>
     </>
   );
