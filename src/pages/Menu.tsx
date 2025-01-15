ba0f27f98ace2c473f7ec0a0ad6850d5cc60d561
Ala Hojat <alahojatjalali@gmail.com>
2025-01-15 14:00:41 +0100

    chore: :art: add menu adjustment for placements

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import defaultLogo from "../assets/artin-hojat-steel.svg";
-import lightLogo from "../assets/artin-hojat-fill-orange-bg.svg";
+import defaultLogo from "../assets/site-logo/artin-hojat-steel.svg";
+import lightLogo from "../assets/site-logo/artin-hojat-fill-orange-bg.svg";
 
 export const Menu = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -28,7 +28,7 @@ export const Menu = () => {
   return (
     <>
       <>
-        <header className="z-20 w-full sm:absolute sm:left-0 sm:top-0 md:flex md:h-16 md:items-center md:justify-between md:bg-chalk md:px-8 md:py-2">
+        <header className="z-20 w-full sm:absolute sm:left-0 sm:top-0 md:flex md:h-20 md:items-center md:justify-between md:bg-snow md:px-8 md:py-2">
           <section className="mr-5 mt-5 flex justify-end md:hidden">
             <div
               className={`menu-button ${menuIsOpen ? "menu-open" : ""}`}
@@ -50,18 +50,18 @@ export const Menu = () => {
           </section>
 
           <nav
-            className={`fixed right-0 top-0 flex h-full w-full transform flex-col ${
+            className={`col fixed right-0 top-0 h-full w-full transform ${
               menuIsOpen ? "translate-x-0" : "translate-x-full"
             } z-20 transition-transform duration-300 md:static md:h-auto md:w-full md:translate-x-0 md:flex-row md:items-center md:justify-between`}
           >
             <div className="m-4 flex justify-end md:hidden">
               <div
-                className="flex h-16 w-16 items-center justify-center rounded-full border-4 border-orange"
+                className="flex h-16 w-16 items-center justify-center"
                 onClick={toggleMenu}
               ></div>
             </div>
 
-            <ul className="menu pr-4 text-right md:flex md:space-x-6 md:pr-0 md:text-left">
+            <ul className="menu md:row justify-evenly p-4 sm:text-right md:flex md:items-start md:justify-start md:gap-4 md:text-left lg:gap-12">
               <li>
                 <NavLink to={"/"} onClick={handleNavLinkClick}>
                   Home
@@ -79,7 +79,7 @@ export const Menu = () => {
               </li>
               <li>
                 <NavLink to={"/articles"} onClick={handleNavLinkClick}>
-                  The Art in Science
+                  The art in Science
                 </NavLink>
               </li>
               <li>
@@ -110,7 +110,7 @@ export const Menu = () => {
             <img
               src={logo}
               alt={"artin hojat logo"}
-              className="hidden h-16 w-16 cursor-pointer md:block"
+              className="ml-4 hidden h-16 w-16 cursor-pointer md:block"
             />
           </nav>
         </header>
