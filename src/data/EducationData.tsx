556b26cc799aca8944487298fa6de87a6636bb80
Ala Hojat <alahojatjalali@gmail.com>
2025-01-15 14:04:53 +0100

    chore: :art: add data for meritspage

---
+++ src/data/EducationData.tsx
@@ -0,0 +1,52 @@
+export const educationData = [
+  {
+    id: 1,
+    level: "Undergraduate",
+    degree: "Bachelor's Degree in Medical Science - Physical Therapy",
+    institution: "Karolinska Institute",
+    location: "Stockholm, Sweden",
+    dates: "2021 - 2024",
+    description:
+      "Graduating with a degree in medicine as a Physical Therapist. Extended studies in Psychology, Neuropsychology, and Physiology from 2020 onwards.",
+  },
+  {
+    id: 2,
+    level: "Undergraduate",
+    degree: "Bachelor of Science in Business Administration → Management",
+    institution: "Stockholm University",
+    location: "Stockholm, Sweden",
+    dates: "2014 - 2017",
+    description:
+      "Graduating with a degree in science in business administration. Focused studies in management and personnel health.",
+  },
+  {
+    id: 3,
+    level: "Graduate",
+    degree:
+      "Master's Degree in Medicine → Physical Therapy & Behavioural Medicine",
+    institution: "Karolinska Institute",
+    location: "Stockholm, Sweden",
+    dates: "2024 - Current",
+    description:
+      "A master’s degree formed after psychological sciences for physical therapy and behavioral medicine.",
+  },
+  {
+    id: 4,
+    level: "Graduate",
+    degree: "Master's Degree in Medicine → Ergonomics",
+    institution: "Karolinska Institute",
+    location: "Stockholm, Sweden",
+    dates: "2024 - Current",
+    description:
+      "A master’s degree in health and work sciences, specialized in ergonomics.",
+  },
+  {
+    id: 5,
+    level: "Certification",
+    title: "Personal Trainer Education",
+    institution: "The Academy",
+    location: "",
+    dates: "",
+    description: "Completed professional certification for personal training.",
+  },
+];
---
+++ src/data/WorkExperienceData.tsx
@@ -0,0 +1,56 @@
+export const workExperienceData = [
+  {
+    id: 1,
+    title: "Player Development Trainer/Basketball and Athletics",
+    employmentType: "Self Employed",
+    location: "",
+    dates: "Present",
+    description:
+      "Player Development in Basketball, working with clients in the NBA, ACB League, WNBA, NCAA, and other professional European Basketball Leagues.",
+  },
+  {
+    id: 2,
+    title: "Health & Performance Consultant",
+    employmentType: "Self Employed",
+    location: "",
+    dates: "Present",
+    description:
+      "Optimizing health and using psychology for improving work and health for individuals and organizations.",
+  },
+  {
+    id: 3,
+    title: "Writer, Media and Event Planning",
+    company: "Kingsize Magazine",
+    location: "Stockholm, Sweden",
+    dates: "2014 - 2020",
+    description:
+      "Video Games, Sports, TV-Shows/Movies, Music, and Popular Culture writing and planning, hosting community events with video game companies. Interview credits include Taraji P. Henson, Big Sean, Lupe Fiasco, and many more.",
+  },
+  {
+    id: 4,
+    title: "Physical Therapy Intern",
+    company: "OrthoSport",
+    location: "Davie, Florida",
+    dates: "Fall 2023",
+    description:
+      "Internship at an orthopedic physical therapy clinic, working with physical therapy practices.",
+  },
+  {
+    id: 5,
+    title: "Head of Basketball Operations, Personal Trainer",
+    company: "SATS Sports Club",
+    location: "Stockholm, Sweden",
+    dates: "2019 - 2022",
+    description:
+      "Developed and led the Basketball Program for SATS Sports Club as the first commercialized franchised Basketball & Strength Training Facility in Sweden.",
+  },
+  {
+    id: 6,
+    title: "Coordinator for NBA 2K Events",
+    employmentType: "Event Planning and Management",
+    location: "Stockholm, Sweden",
+    dates: "2015 - 2020",
+    description:
+      "Coordinated and hosted NBA 2K events in Sweden for the annual releases of the video game.",
+  },
+];
