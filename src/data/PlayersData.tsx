be99c7faa8da2ea31656cf39339c1784caebd6ad
Ala Hojat <alahojatjalali@gmail.com>
2025-01-15 14:12:34 +0100

    chore: :art: add updates to playerdata and image imports

--- src/data/PlayersData.tsx
+++ src/data/PlayersData.tsx
@@ -1,65 +1,71 @@
-import melwin from "../assets/melwin-pantzar.png";
-import elliot from "../assets/elliot.jpg";
-import william from "../assets/william.png";
-import amanda from "../assets/amanda-zahui.jpg";
-import bobi from "../assets/bobi-klintman.jpg";
-import pelle from "../assets/pelle-larsson.jpg";
+import melwin from "../assets/players/melwin-pantzar.png";
+import elliot from "../assets/players/elliot.jpg";
+import ludde from "../assets/players/ludde-hakanson.jpg";
+import amanda from "../assets/players/amanda-zahui.jpg";
+import bobi from "../assets/players/bobi-klintman.jpg";
+import pelle from "../assets/players/pelle-larsson.png";
 
 export const players = [
   {
-    id: 1,
-    title: "Elliot Cadeau",
-    description: "Skill Training, Strength and conditioning ",
-    url: elliot,
-    thumbnail: "/assets/images/thumbnails/city-skyline_thumb.jpg",
+    id: 4,
+    title: "Amanda Zahui B.",
+    description:
+      "Player development, skill-training, strength and conditioning, psychological training and nutrition optimization",
+    url: amanda,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
     tags: ["basketball", "nba", "artinhojat"],
-    photographer: "Ala Hojat",
+    photographer: "Andrés Alfvin",
   },
+
   {
     id: 2,
     title: "Melwin Pantzar",
-    description: "Skill Training, Strength and conditioning ",
+    description:
+      "Focused on enhancing skill-training, strength and conditioning, mental performance, and nutrition optimization.",
     url: melwin,
     thumbnail: "/assets/images/thumbnails/sunset_thumb.jpg",
     tags: ["basketball", "nba", "artinhojat"],
     alt: "image",
     photographer: "Ala Hojat",
   },
-
   {
-    id: 3,
-    title: "William",
-    description: "Skill Training, Strength and conditioning ",
-    url: william,
-    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    id: 1,
+    title: "Elliot Cadeau",
+    description:
+      "Helped in the establishment Elliot's brand in Sweden with partnerships, off-season skill-training",
+    url: elliot,
+    thumbnail: "/assets/images/thumbnails/city-skyline_thumb.jpg",
     tags: ["basketball", "nba", "artinhojat"],
     photographer: "Ala Hojat",
   },
   {
-    id: 4,
-    title: "Amanda Zahui B.",
-    description: "Skill Training, Strength and conditioning ",
-    url: amanda,
-    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
-    tags: ["basketball", "nba", "artinhojat"],
-    photographer: "?",
-  },
-  {
-    id: 5,
-    title: "Bobi Klintman",
-    description: "Skill Training, Strength and conditioning ",
-    url: bobi,
-    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
-    tags: ["basketball", "nba", "artinhojat"],
-    photographer: "?",
-  },
-  {
     id: 6,
     title: "Pelle Larsson",
-    description: "Skill Training, Strength and conditioning ",
+    description:
+      "Off-season skill training, in season council and player-development",
     url: pelle,
     thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
     tags: ["basketball", "nba", "artinhojat"],
-    photographer: "?",
+    photographer: "Ala Hojat",
+  },
+
+  {
+    id: 3,
+    title: "Ludvig Håkanson",
+    description: "Off-season skill-training",
+    url: ludde,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "Andrés Alfvin",
+  },
+
+  {
+    id: 5,
+    title: "Bobi Klintman",
+    description: "Player development council",
+    url: bobi,
+    thumbnail: "/assets/images/thumbnails/beach_thumb.jpg",
+    tags: ["basketball", "nba", "artinhojat"],
+    photographer: "Joel Rosén",
   },
 ];
