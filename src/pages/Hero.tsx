00664e0a115aa073237aea4470e88730bc3a2b26
Ala Hojat <alahojatjalali@gmail.com>
2025-01-15 16:40:03 +0100

    chore: :construction: add logo to hero page

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,7 +1,11 @@
+import logo from "../assets/site-logo/artin-hojat-fill-orange-bg.svg";
+
 export const Hero = ({ onDismiss }: { onDismiss: () => void }) => {
   return (
-    <section className="hero" onClick={onDismiss}>
-      <h1 className="header">Hero</h1>
+    <section className="hero col items-start justify-start" onClick={onDismiss}>
+      <div className="z-20 m-20">
+        <img src={logo} alt="" className="h-auto w-[500px] cursor-pointer" />
+      </div>
     </section>
   );
 };
