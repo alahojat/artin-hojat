88ec4866495332f9427f3236879dfcf5f7c7bb60
Ala Hojat <alahojatjalali@gmail.com>
2025-01-15 18:46:06 +0100

    chore: :art: add article model

---
+++ src/models/IArticles.ts
@@ -0,0 +1,14 @@
+export interface IArticle {
+  id: number;
+  slug: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  excerpt: {
+    rendered: string;
+  };
+  jetpack_featured_media_url: string;
+}
