0d8d03019c9c59c1c8c32ace6a712a268af71d19
Ala Hojat <alahojatjalali@gmail.com>
2025-01-17 13:35:11 +0100

    chore: :art: add updated logic using the api response instead of the mockdata for rendering articles

--- src/components/ArticleCards.tsx
+++ src/components/ArticleCards.tsx
@@ -1,26 +1,33 @@
-import { NavLink } from "react-router-dom";
-import { articles } from "../data/MockArticlesData";
+import { IArticlesResponse } from "../models/IArticles";
 
-export const ArticleCards = () => {
+interface IDisplayArticlesProps {
+  articles: IArticlesResponse;
+}
+
+export const ArticleCards = ({ articles }: IDisplayArticlesProps) => {
   const handleArticleClick = () => {
     console.log("is this working");
-    <NavLink to={"/articles/:id"}></NavLink>;
   };
   return (
-    <section
-      className="grid cursor-pointer grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4"
-      onClick={handleArticleClick}
-    >
+    <section className="grid cursor-pointer grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4">
       {articles.map((article) => (
         <div key={article.id} className="article-card mt-6 p-2">
-          <p className="body m-0 pb-2">{article.dateCreated}</p>
+          <p className="body m-0 pb-2">
+            {new Date(article.date).toLocaleDateString("en-GB", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })}
+          </p>
           <img
-            src={article.img}
-            alt={article.title}
+            src={article.jetpack_featured_media_url}
+            alt={article.title.rendered}
             className="w-full object-cover sm:h-52 lg:h-72"
           />
-          <h3 className="subheading-alt">{article.title}</h3>
-          <p className="body">{article.body}</p>
+          <h3 onClick={handleArticleClick} className="subheading-alt">
+            {article.title.rendered}
+          </h3>
+          <p className="body">{article.excerpt.rendered}</p>
         </div>
       ))}
     </section>
