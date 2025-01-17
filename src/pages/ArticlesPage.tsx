399f366c5bad8cf681a98843a7266e9fa812054d
Ala Hojat <alahojatjalali@gmail.com>
2025-01-17 13:32:27 +0100

    chore: :art: add the dataloader in the article page

--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -1,11 +1,14 @@
+import { useLoaderData } from "react-router-dom";
 import { ArticleCards } from "../components/ArticleCards";
+import { IArticlesResponse } from "../models/IArticles";
 
 export const ArticlesPage = () => {
+  const articles = useLoaderData() as IArticlesResponse;
   return (
     <>
       <section className="base bg-ice">
         <h2 className="subheading">The Art in Science</h2>
-        <ArticleCards></ArticleCards>
+        <ArticleCards articles={articles}></ArticleCards>
       </section>
     </>
   );
