666118b352244a833c07621bb1deaf8bac7b217c
Ala Hojat <alahojatjalali@gmail.com>
2025-01-20 12:22:58 +0100

    chore: :art: add interface to include images from api

--- src/models/IArticles.ts
+++ src/models/IArticles.ts
@@ -12,6 +12,18 @@ export interface IArticle {
     rendered: string;
   };
   jetpack_featured_media_url: string;
+  featured_media: number;
 }
 
 export type IArticlesResponse = IArticle[];
+
+export interface IMedia {
+  id: number;
+  caption: {
+    rendered: string;
+  };
+  description: {
+    rendered: string;
+  };
+  alt_text: string;
+}
