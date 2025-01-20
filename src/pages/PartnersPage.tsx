5e197ab45cebabd19ac8f9a5bfa0dd8d20986603
Ala Hojat <alahojatjalali@gmail.com>
2025-01-20 13:56:16 +0100

    chore: :zap: add loading lazy to partner logos

--- src/pages/PartnersPage.tsx
+++ src/pages/PartnersPage.tsx
@@ -20,61 +20,115 @@ export const PartnersPage = () => {
         <section className="grid grid-cols-2 lg:grid-cols-5">
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://nocco.com/sv/">
-              <img src={nocco} alt="" width="80px" />
+              <img
+                src={nocco}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://www.betteryou.se/">
-              <img src={betteryou} alt="" width="90px" />
+              <img
+                src={betteryou}
+                alt=""
+                width="90px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://www.flowlife.com/se/">
-              <img src={flowlife} alt="" width="80px" />
+              <img
+                src={flowlife}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://www.elliotcadeau.com/">
-              <img src={elliot} alt="" width="80px" />
+              <img
+                src={elliot}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://www.sats.se/gym/ursvik">
-              <img src={sats} alt="" width="80px" />
+              <img
+                src={sats}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://en.bandainamcoent.eu/">
-              <img src={bandai} alt="" width="80px" />
+              <img
+                src={bandai}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://nba.2k.com/">
-              <img src={nba} alt="" width="80px" />
+              <img src={nba} alt="" width="80px" height="auto" loading="lazy" />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://www.comicconstockholm.se/en/">
-              <img src={comicCon} alt="" width="80px" />
+              <img
+                src={comicCon}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://www.kingsizemag.se/">
-              <img src={kingsize} alt="" width="80px" />
+              <img
+                src={kingsize}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
 
           <article className="row m-2 items-center justify-center gap-4">
             <a href="https://ng.se/stockholm">
-              <img src={nojesguiden} alt="" width="80px" />
+              <img
+                src={nojesguiden}
+                alt=""
+                width="80px"
+                height="auto"
+                loading="lazy"
+              />
             </a>
           </article>
         </section>
