ef559ff95a69d4dc21419cf2bbe69ccd9bb6d53e
Ala Hojat <alahojatjalali@gmail.com>
2025-01-20 13:42:55 +0100

    chore: :card_file_box: add fetch for article media

--- src/services/articleService.ts
+++ src/services/articleService.ts
@@ -1,26 +1,45 @@
-import { IArticle, IArticlesResponse } from "../models/IArticles";
-
+import { IArticle, IArticlesResponse, IMedia } from "../models/IArticles";
 import { get } from "./serviceBase";
 
 const BASE_URL = "http://13.53.176.130/wp-json/wp/v2/posts";
+const MEDIA_URL = "http://13.53.176.130/wp-json/wp/v2/media";
 
+// Fetch all articles
 export const getArticles = async (): Promise<IArticlesResponse> => {
   try {
     const response = await get<IArticlesResponse>(BASE_URL);
-
     return response.data;
   } catch (error) {
-    console.error("Error fetching profile:", error);
+    console.error("Error fetching articles:", error);
     throw error;
   }
 };
 
-export const getArticleById = async (id: number): Promise<IArticle> => {
+export const getArticleById = async (
+  id: number,
+): Promise<IArticle & { media?: IMedia }> => {
   try {
     const response = await get<IArticle>(`${BASE_URL}/${id}`);
-    return response.data;
+    const article = response.data;
+
+    if (article.featured_media) {
+      const media = await getArticleMedia(article.featured_media);
+      return { ...article, media };
+    }
+
+    return article;
   } catch (error) {
     console.error(`Error fetching article with ID ${id}:`, error);
+    throw error;
+  }
+};
+
+export const getArticleMedia = async (mediaId: number): Promise<IMedia> => {
+  try {
+    const response = await get<IMedia>(`${MEDIA_URL}/${mediaId}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching media with ID ${mediaId}:`, error);
     throw error;
   }
 };
