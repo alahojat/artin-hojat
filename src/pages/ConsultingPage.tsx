ade593272c84461d051f0d6f660a48b6038e0b07
Ala Hojat <alahojatjalali@gmail.com>
2025-01-20 13:08:08 +0100

    chore: :art: add consultingpage content to contactpage

--- src/pages/ConsultingPage.tsx
+++
@@ -1,9 +0,0 @@
-export const ConsultingPage = () => {
-  return (
-    <>
-      <section className="base bg-ice">
-        <h2 className="subheading">Consulting</h2>
-      </section>
-    </>
-  );
-};
