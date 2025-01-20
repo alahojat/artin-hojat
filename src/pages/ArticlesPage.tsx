c8024d73eb9b0b129948fa802e6e14f6caaabefe
Ala Hojat <alahojatjalali@gmail.com>
2025-01-20 13:43:57 +0100

    style: :lipstick: add misc changes to image styling and containers

--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -6,8 +6,10 @@ export const ArticlesPage = () => {
   const articles = useLoaderData() as IArticlesResponse;
   return (
     <>
-      <section className="base bg-ice">
-        <h2 className="subheading">The Art in Science</h2>
+      <section className="base bg-chalk">
+        <div className="lg:row mb-6 lg:items-center lg:justify-between">
+          <h2 className="subheading">The Art in Science</h2>
+        </div>
         <ArticleCards articles={articles}></ArticleCards>
       </section>
     </>
