ee9e8ab6c5643420b671ffc0606e3f16dc540077
Ala Hojat <alahojatjalali@gmail.com>
2025-01-20 14:01:44 +0100

    style: :lipstick: add layout updates for articlecards and update link to single article

--- src/components/ArticleCards.tsx
+++ src/components/ArticleCards.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { IArticlesResponse } from "../models/IArticles";
 import parse from "html-react-parser";
 
@@ -6,14 +7,11 @@ interface IDisplayArticlesProps {
 }
 
 export const ArticleCards = ({ articles }: IDisplayArticlesProps) => {
-  const handleArticleClick = () => {
-    console.log("is this working");
-  };
   return (
-    <section className="grid cursor-pointer grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4">
+    <section className="grid grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4">
       {articles.map((article) => (
         <div key={article.id} className="article-card mt-6 p-2">
-          <p className="body m-0 pb-2">
+          <p className="body-alt inline rounded bg-orange px-2">
             {new Date(article.date).toLocaleDateString("en-GB", {
               day: "2-digit",
               month: "long",
@@ -23,12 +21,16 @@ export const ArticleCards = ({ articles }: IDisplayArticlesProps) => {
           <img
             src={article.jetpack_featured_media_url}
             alt={article.title.rendered}
-            className="w-full object-cover sm:h-52 lg:h-72"
+            loading="lazy"
+            className="my-2 w-full object-cover sm:h-52 lg:h-72"
           />
-          <h3 onClick={handleArticleClick} className="subheading-alt">
+          <Link
+            to={`/articles/${article.id}`}
+            className="subheading-alt cursor-pointer hover:text-midnight hover:underline"
+          >
             {article.title.rendered}
-          </h3>
-          <p className="body">{parse(article.excerpt.rendered)}</p>
+          </Link>
+          <article className="body">{parse(article.excerpt.rendered)}</article>
         </div>
       ))}
     </section>
