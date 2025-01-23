561ac9878121876c102b1a09164adcce9b6f0994
Ala Hojat <alahojatjalali@gmail.com>
2025-01-23 13:15:23 +0100

    chore: :art: add animation for articles and handle scroll position

--- src/components/ArticleCards.tsx
+++ src/components/ArticleCards.tsx
@@ -7,10 +7,16 @@ interface IArticlesCardProps {
 }
 
 export const ArticleCards = ({ articles }: IArticlesCardProps) => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
   return (
     <section className="grid grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4">
       {articles.map((article) => (
-        <div key={article.id} className="article-card mt-6 p-2">
+        <div
+          key={article.id}
+          className="article-card intersect:motion-preset-fade mt-6 p-2"
+        >
           <p className="body-alt body-alt-light inline rounded bg-midnight px-2">
             {new Date(article.date).toLocaleDateString("en-GB", {
               day: "2-digit",
@@ -27,6 +33,7 @@ export const ArticleCards = ({ articles }: IArticlesCardProps) => {
           <Link
             to={`/articles/${article.id}`}
             className="subheading-alt cursor-pointer hover:text-midnight hover:underline"
+            onClick={handleClick}
           >
             {article.title.rendered}
           </Link>
