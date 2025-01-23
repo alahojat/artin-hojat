1fa6bddbd3808b3cf262f80497a63eb73ba82b04
Ala Hojat <alahojatjalali@gmail.com>
2025-01-23 13:20:49 +0100

    feat: :sparkles: add spotify embedded widget

---
+++ src/components/SpotifyEmbed.tsx
@@ -0,0 +1,17 @@
+export const SpotifyEmbed = () => {
+  const iframeSrc = `https://open.spotify.com/embed/show/5JTNlIk5jpGpZf0Ri6b84N?theme=0`;
+
+  return (
+    <div>
+      <iframe
+        src={iframeSrc}
+        width="100%"
+        height="200"
+        allow="encrypted-media"
+        title="Spotify Show Player"
+        loading="lazy"
+        className="max-w-[500px]"
+      ></iframe>
+    </div>
+  );
+};
