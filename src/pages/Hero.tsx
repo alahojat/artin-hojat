3ffffced6fcad7ad20abc25a3da23b146db9bdf3
Ala Hojat <alahojatjalali@gmail.com>
2025-01-23 13:28:11 +0100

    chore: :dizzy: add animation for hero page logo

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -3,12 +3,12 @@ import logo from "../assets/site-logo/artin-hojat-fill-orange-bg.svg";
 export const Hero = ({ onDismiss }: { onDismiss: () => void }) => {
   return (
     <section className="hero col items-start justify-start" onClick={onDismiss}>
-      <div className="z-20 m-20">
+      <div className="motion-preset-focus motion-duration-2000 z-20">
         <img
           src={logo}
           alt=""
           loading="lazy"
-          className="h-auto w-[500px] cursor-pointer"
+          className="h-auto w-[200px] cursor-pointer"
         />
       </div>
     </section>
