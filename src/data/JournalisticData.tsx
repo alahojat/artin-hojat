e35d193b66e2eaf1b5332ba59ff0dfae3c47c7b0
Ala Hojat <alahojatjalali@gmail.com>
2025-01-23 13:18:43 +0100

    chore: :card_file_box: add namelist for meritspage

---
+++ src/data/JournalisticData.tsx
@@ -0,0 +1,157 @@
+export const journalisticData = [
+  {
+    id: 1,
+    name: "6lack",
+    category: "artist",
+  },
+  {
+    id: 2,
+    name: "Big Sean",
+    category: "artist",
+  },
+  {
+    id: 3,
+    name: "Dave East",
+    category: "artist",
+  },
+  {
+    id: 4,
+    name: "Denzel Curry",
+    category: "artist",
+  },
+  {
+    id: 5,
+    name: "Dvsn",
+    category: "artist",
+  },
+  {
+    id: 6,
+    name: "Isaiah Rashad",
+    category: "artist",
+  },
+  {
+    id: 7,
+    name: "Jireel",
+    category: "artist",
+  },
+  {
+    id: 8,
+    name: "Jorja Smith",
+    category: "artist",
+  },
+  {
+    id: 9,
+    name: "Lupe Fiasco",
+    category: "artist",
+  },
+  {
+    id: 10,
+    name: "Majid Jordan",
+    category: "artist",
+  },
+  {
+    id: 11,
+    name: "Roy Woods",
+    category: "artist",
+  },
+  {
+    id: 12,
+    name: "Rich The Kid",
+    category: "artist",
+  },
+  {
+    id: 13,
+    name: "Snoh Aalegra",
+    category: "artist",
+  },
+  {
+    id: 14,
+    name: "The Sugarhill Gang",
+    category: "artist",
+  },
+  {
+    id: 15,
+    name: "Yazz",
+    category: "artist",
+  },
+  {
+    id: 16,
+    name: "Alexander Karim",
+    category: "actor",
+  },
+  {
+    id: 17,
+    name: "Grace Byers",
+    category: "actor",
+  },
+  {
+    id: 18,
+    name: "Taraji P. Henson",
+    category: "actor",
+  },
+  {
+    id: 19,
+    name: "Trai Byers",
+    category: "actor",
+  },
+  {
+    id: 20,
+    name: "Bobo Klintman",
+    category: "athlete",
+  },
+  {
+    id: 21,
+    name: "Danny Green",
+    category: "athlete",
+  },
+  {
+    id: 22,
+    name: "DeMarcus Cousins",
+    category: "athlete",
+  },
+  {
+    id: 23,
+    name: "Dejan Kulusevski",
+    category: "athlete",
+  },
+  {
+    id: 24,
+    name: "Davis Bertans",
+    category: "athlete",
+  },
+  {
+    id: 25,
+    name: "Elliot Cadeau",
+    category: "athlete",
+  },
+  {
+    id: 26,
+    name: "Pelle Larsson",
+    category: "athlete",
+  },
+  {
+    id: 27,
+    name: "Jonas Jerebko",
+    category: "athlete",
+  },
+  {
+    id: 28,
+    name: "Jordan Genmark Heath",
+    category: "athlete",
+  },
+  {
+    id: 29,
+    name: "Usman Garuba",
+    category: "athlete",
+  },
+  {
+    id: 30,
+    name: "Robin Safar",
+    category: "athlete",
+  },
+  {
+    id: 31,
+    name: "Sadibou Sy",
+    category: "athlete",
+  },
+];
