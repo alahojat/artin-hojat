074c37cdc830977fe2f4842093690363fa7cb2b0
Ala Hojat <alahojatjalali@gmail.com>
2025-01-23 13:30:53 +0100

    chore: :construction: add draft content for homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,39 @@
+import { SpotifyEmbed } from "../components/SpotifyEmbed";
+import artinhojat2 from "../assets/artin-hojat-3.jpg";
+import jimmy from "../assets/article-img/jimmy.jpeg";
+import { NavLink } from "react-router-dom";
+
 export const Home = () => {
   return (
     <>
-      <div className="w-full flex-grow items-center bg-ice pt-24 sm:px-4 sm:pb-16 lg:px-6 lg:pb-16 lg:pt-32">
-        <section className="lg:max-w-[1300px]">
-          <section className="lg:w-[1200px]">
-            <h3 className="word font-bold uppercase text-midnight lg:text-6xl">
-              The art in Science
-            </h3>
-            <h3 className="word font-bold uppercase text-midnight lg:text-7xl">
-              Psychology
-            </h3>
-            <h3 className="word font-bold uppercase text-midnight lg:text-4xl">
-              Development
-            </h3>
-          </section>
+      <div className="w-full max-w-[1470px] flex-grow items-center bg-ice pt-24 sm:pb-16 md:mt-4 lg:pb-16">
+        <section className="bg-chalk">Hello</section>
+
+        <section className="flex-grow bg-steel">
+          <div className="orange-circle right-4"></div>
+          <NavLink
+            to={"/articles"}
+            className="subheading left-12 top-20 text-orange"
+          >
+            The art in Science
+          </NavLink>
+          <h2 className="subheading subheading-light left-8 top-24">
+            Psychology
+          </h2>
+        </section>
+        <section className="flex-grow bg-midnight">
+          <img
+            src={artinhojat2}
+            alt=""
+            className="motion-preset-fade motion-duration-3000"
+          />
+
+          <img
+            src={jimmy}
+            alt=""
+            className="motion-preset-fade motion-duration-3000"
+          />
+          <SpotifyEmbed></SpotifyEmbed>
         </section>
       </div>
     </>
