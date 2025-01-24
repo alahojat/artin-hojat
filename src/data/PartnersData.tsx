1ea1045402d023f1a5c71be81b30cab59c7532fa
Ala Hojat <alahojatjalali@gmail.com>
2025-01-24 21:07:31 +0100

    feat: :sparkles: add playersdata in own component

---
+++ src/data/PartnersData.tsx
@@ -0,0 +1,93 @@
+import nocco from "/brand-logos/nocco.png";
+import betteryou from "/brand-logos/betteryou-logo.webp";
+import flowlife from "/brand-logos/flowlife.avif";
+import elliot from "/brand-logos/elliot-cadeau.png";
+import bandai from "/brand-logos/bandai-namco.svg";
+import comicCon from "/brand-logos/comic-con.png";
+import nba from "/brand-logos/nba.png";
+import sats from "/brand-logos/sats.png";
+import kingsize from "/brand-logos/kingsize.svg";
+import nojesguiden from "/brand-logos/nojesguiden.png";
+
+export const PartnersData = [
+  {
+    id: 1,
+    href: "https://nocco.com/sv/",
+    src: nocco,
+    alt: "Logo for Nocco",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 2,
+    href: "https://www.betteryou.se/",
+    src: betteryou,
+    alt: "Logo for Better You",
+    width: "90px",
+    height: "auto",
+  },
+  {
+    id: 3,
+    href: "https://www.flowlife.com/se/",
+    src: flowlife,
+    alt: "Logo for Flowlife",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 4,
+    href: "https://www.elliotcadeau.com/",
+    src: elliot,
+    alt: "Logo for Elliot Cadeau",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 5,
+    href: "https://www.sats.se/gym/ursvik",
+    src: sats,
+    alt: "Logo for SATS",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 6,
+    href: "https://en.bandainamcoent.eu/",
+    src: bandai,
+    alt: "Logo for Bandai Namco",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 7,
+    href: "https://nba.2k.com/",
+    src: nba,
+    alt: "Logo for NBA 2K",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 8,
+    href: "https://www.comicconstockholm.se/en/",
+    src: comicCon,
+    alt: "Logo for Comic Con Stockholm",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 9,
+    href: "https://www.kingsizemag.se/",
+    src: kingsize,
+    alt: "Logo for Kingsize Magazine",
+    width: "80px",
+    height: "auto",
+  },
+  {
+    id: 10,
+    href: "https://ng.se/stockholm",
+    src: nojesguiden,
+    alt: "Logo for Nöjesguiden",
+    width: "80px",
+    height: "auto",
+  },
+];
