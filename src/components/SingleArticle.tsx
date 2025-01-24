22bd1688686f4f39035edd4190a969684f1aff97
Ala Hojat <alahojatjalali@gmail.com>
2025-01-24 20:59:52 +0100

    chore: :art: replace images with styled img component

--- src/components/SingleArticle.tsx
+++ src/components/SingleArticle.tsx
@@ -4,6 +4,7 @@ import { cleanContent } from "../utils/htmlParser";
 import striptags from "striptags";
 import parse from "html-react-parser";
 import { GoBackBtn } from "./GoBackBtn";
+import { StyledImg } from "./StyledImg";
 
 export const SingleArticle = () => {
   const singleArticle = useLoaderData() as IArticle & { media: IMedia };
@@ -11,33 +12,28 @@ export const SingleArticle = () => {
   return (
     <>
       <GoBackBtn></GoBackBtn>
-      <article>
+      <article className="bg-ice pb-6 pt-2 lg:px-6">
         <div key={singleArticle.id}>
           <h2 className="article-heading font-bold lg:subheading-lg">
-            {singleArticle.title.rendered}
+            {parse(singleArticle.title.rendered)}
           </h2>
-
           <div className="lg:grid lg:grid-cols-3 lg:gap-6">
-            <div className="lg:col-span-1">
-              <img
-                src={singleArticle.jetpack_featured_media_url}
-                alt={singleArticle.media.alt_text}
-                loading="lazy"
-                className="w-full object-cover pb-2 sm:h-96 md:w-96 lg:h-96"
-              />
-              <span className="photocred">
-                Photo by: {striptags(singleArticle.media.caption.rendered)}
-              </span>
-            </div>
+            <StyledImg
+              src={singleArticle.jetpack_featured_media_url}
+              alt={singleArticle.media.alt_text}
+              imgClassName="pb-2 sm:h-96 md:w-96 lg:h-96"
+              figClassName="lg:col-span-1"
+              photocred={striptags(singleArticle.media.caption.rendered)}
+            ></StyledImg>
             <section className="sm:my-4 lg:col-span-2 lg:my-0">
-              <p className="body-alt body-alt-light inline rounded bg-midnight-low px-2">
+              <p className="body-alt inline">
                 {new Date(singleArticle.date).toLocaleDateString("en-GB", {
                   day: "2-digit",
                   month: "long",
                   year: "numeric",
                 })}
               </p>
-              <article className="excerpt mt-4">
+              <article className="excerpt mt-2">
                 {parse(singleArticle.excerpt.rendered)}
               </article>
               <article>{cleanContent(singleArticle.content.rendered)}</article>
