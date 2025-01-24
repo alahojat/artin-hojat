b0c54a3ef0e2feb9b2d92743688940061cf525ed
Ala Hojat <alahojatjalali@gmail.com>
2025-01-24 21:05:52 +0100

    style: :art: add new footer structure

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -7,80 +7,87 @@ import { PiLinkedinLogoThin } from "react-icons/pi";
 
 export const Footer = () => {
   return (
-    <footer className="col bg-orange sm:h-[500px] md:h-[550] lg:h-[350px] lg:w-full lg:pb-4">
-      <div className="flex-shrink-0 bg-midnight">
+    <footer className="col bg-purple sm:h-[450px] md:h-[350px] lg:h-[400px] lg:w-full lg:pb-4">
+      <div className="flex-shrink-0 bg-orange">
         <ScrollingBanner />
       </div>
 
-      <section className="sm:col lg:row mx-4 flex-1 items-center justify-center md:flex md:gap-6 lg:mx-auto lg:max-w-[1100px] lg:items-start">
-        <div className="my-8 flex items-center justify-center">
+      <div className="col md:row mx-4 flex-1 items-center justify-center sm:gap-8 lg:gap-80">
+        <div className="mx-2 grid grid-cols-2 sm:gap-20 md:gap-24 lg:max-w-[1100px]">
+          <ul className="footer a space-y-2 text-left text-steel">
+            <li className="hover:text-steel">
+              <a href="/" className="whitespace-nowrap">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/articles" className="whitespace-nowrap">
+                The Art in Science
+              </a>
+            </li>
+            <li>
+              <a href="/player-optimization" className="whitespace-nowrap">
+                Player optimization
+              </a>
+            </li>
+          </ul>
+          <ul className="footer a space-y-2 text-left text-steel">
+            <li>
+              <a href="/contact" className="whitespace-nowrap">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="whitespace-nowrap">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/merits" className="whitespace-nowrap">
+                Merits
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center justify-center">
           <img
             className="md:w-30 h-auto sm:w-28 lg:w-40"
             src={circleLogo}
             alt="site logo"
           />
         </div>
+      </div>
 
-        <div className="mx-4 grid grid-cols-2 gap-12 md:gap-24 lg:mt-8">
-          <ul className="footer a space-y-2">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/articles">The Art in Science</a>
-            </li>
-            <li>
-              <a href="/player-optimization">Player optimization</a>
-            </li>
-          </ul>
-          <ul className="footer a space-y-2">
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/merits">Merits</a>
-            </li>
-            <li>
-              <a href="/partners">Partners</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="row mt-8 justify-center gap-6">
-          <a
-            href="https://www.instagram.com/artinhojatg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <CiInstagram className="text-3xl text-steel" />
-          </a>
-          <a
-            href="https://www.tiktok.com/@artinhojat"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <PiTiktokLogoLight className="text-3xl text-steel" />
-          </a>
-          <a
-            href="https://www.youtube.com/@TheArtinprogress"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <CiYoutube className="text-3xl text-steel" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/artinhojat/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <PiLinkedinLogoThin className="text-3xl text-steel" />
-          </a>
-        </div>
-      </section>
-
+      <div className="row my-2 justify-center gap-6">
+        <a
+          href="https://www.instagram.com/artinhojatg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CiInstagram className="text-3xl text-steel" />
+        </a>
+        <a
+          href="https://www.tiktok.com/@artinhojat"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <PiTiktokLogoLight className="text-3xl text-steel" />
+        </a>
+        <a
+          href="https://www.youtube.com/@TheArtinprogress"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <CiYoutube className="text-3xl text-steel" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/artinhojat/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <PiLinkedinLogoThin className="text-3xl text-steel" />
+        </a>
+      </div>
       <div className="text-center">
         <p className="copyright">
           Copyright &copy; Artin Hojat 2025 All rights reserved
