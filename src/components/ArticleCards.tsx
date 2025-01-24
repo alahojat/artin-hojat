0a3a28a2945a509e2509d895cd958e3c62c914da
Ala Hojat <alahojatjalali@gmail.com>
2025-01-24 21:00:47 +0100

    chore: :art: replace images with styled img component and add link to read more

--- src/components/ArticleCards.tsx
+++ src/components/ArticleCards.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { IArticle } from "../models/IArticles";
 import parse from "html-react-parser";
+import { StyledImg } from "./StyledImg";
 
 interface IArticlesCardProps {
   articles: IArticle[];
@@ -11,33 +12,41 @@ export const ArticleCards = ({ articles }: IArticlesCardProps) => {
     window.scrollTo(0, 0);
   };
   return (
-    <section className="grid grid-cols-1 gap-0 md:grid-cols-3 lg:grid-cols-4">
+    <section className="grid grid-cols-1 gap-0 md:grid-cols-3 lg:mt-4 lg:grid-cols-4">
       {articles.map((article) => (
         <div
           key={article.id}
           className="article-card intersect:motion-preset-fade mt-6 p-2"
         >
-          <p className="body-alt body-alt-light inline rounded bg-midnight px-2">
+          <p className="body-alt inline rounded bg-orange px-2">
             {new Date(article.date).toLocaleDateString("en-GB", {
               day: "2-digit",
               month: "long",
               year: "numeric",
             })}
           </p>
-          <img
+          <StyledImg
             src={article.jetpack_featured_media_url}
             alt={article.title.rendered}
-            loading="lazy"
-            className="my-2 w-full object-cover sm:h-52 lg:h-72"
-          />
+            imgClassName="my-2 sm:h-52 lg:h-72"
+          ></StyledImg>
           <Link
             to={`/articles/${article.id}`}
             className="subheading-alt cursor-pointer hover:text-midnight hover:underline"
             onClick={handleClick}
           >
-            {article.title.rendered}
+            {parse(article.title.rendered)}
           </Link>
           <article className="body">{parse(article.excerpt.rendered)}</article>
+          <Link
+            to={`/articles/${article.id}`}
+            className="cursor-pointer hover:text-midnight hover:underline"
+            onClick={handleClick}
+          >
+            <p className="body-alt m-0 hover:text-midnight hover:underline">
+              Read more
+            </p>
+          </Link>
         </div>
       ))}
     </section>
