4c8da2fdaefbd03295e364463638c83eaf261995
Ala Hojat <alahojatjalali@gmail.com>
2025-01-27 22:12:53 +0100

    chore: :wheelchair: change footer link text for better contrast

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
 
       <div className="col md:row mx-4 flex-1 items-center justify-center sm:gap-8 lg:gap-80">
         <div className="mx-2 grid grid-cols-2 sm:gap-20 md:gap-24 lg:max-w-[1100px]">
-          <ul className="footer a space-y-2 text-left text-steel">
+          <ul className="footer a space-y-2 text-left text-snow">
             <li className="hover:text-steel">
               <a href="/" className="whitespace-nowrap">
                 Home
@@ -31,7 +31,7 @@ export const Footer = () => {
               </a>
             </li>
           </ul>
-          <ul className="footer a space-y-2 text-left text-steel">
+          <ul className="footer a space-y-2 text-left text-snow">
             <li>
               <a href="/contact" className="whitespace-nowrap">
                 Contact
